7e29af1af937d6985d0b8cbace78eb29b668ac5d
Pablo Gonzalez <ezekielgonzalezr@gmail.com>
2023-04-10 14:36:33 -0600

    Fix js linter errors

--- javascript.js
+++ javascript.js
@@ -1,27 +1,27 @@
-let myOpen = document.querySelector(".burger")
-let nav = document.querySelector(".header-menu")
-let myClose = document.querySelector(".close",)
-let myLinks = document.querySelector(".links")
+const myOpen = document.querySelector('.burger');
+const nav = document.querySelector('.header-menu');
+const myClose = document.querySelector('.close');
+const myLinks = document.querySelector('.links');
 
-myOpen.onclick = function(){
-    nav.classList.add("open")
-    myOpen.classList.add("display")
-}
-myClose.onclick = function(){
-    this.parentElement.classList.remove("open")
-    myOpen.classList.remove("display")
-}
-myLinks.onclick = function(){
-    this.parentElement.classList.remove("open")
-    myOpen.classList.remove("display")
-}
+myOpen.onclick = () => {
+  nav.classList.add('open');
+  myOpen.classList.add('display');
+};
+myClose.onclick = () => {
+  this.parentElement.classList.remove('open');
+  myOpen.classList.remove('display');
+};
+myLinks.onclick = () => {
+  this.parentElement.classList.remove('open');
+  myOpen.classList.remove('display');
+};
 const MidLink = document.getElementById('about');
-MidLink.addEventListener('click', function() {
-    this.parentElement.classList.remove("open")
-    myOpen.classList.remove("display")
+MidLink.addEventListener('click', () => {
+  this.parentElement.classList.remove('open');
+  myOpen.classList.remove('display');
 });
 const LastLink = document.getElementById('contact');
-LastLink.addEventListener('click', function() {
-    this.parentElement.classList.remove("open")
-    myOpen.classList.remove("display")
+LastLink.addEventListener('click', () => {
+  this.parentElement.classList.remove('open');
+  myOpen.classList.remove('display');
 });
