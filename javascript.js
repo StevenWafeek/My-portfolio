7448b4eb0b4396b7eaa396ddfacc5cd26596d5f4
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-04-11 12:24:47 +0200

    Fix javascript files

--- javascript.js
+++ javascript.js
@@ -8,20 +8,20 @@ myOpen.onclick = () => {
   myOpen.classList.add('display');
 };
 myClose.onclick = () => {
-  this.parentElement.classList.remove('open');
+  nav.classList.remove('open');
   myOpen.classList.remove('display');
 };
 myLinks.onclick = () => {
-  this.parentElement.classList.remove('open');
+  nav.classList.remove('open');
   myOpen.classList.remove('display');
 };
 const MidLink = document.getElementById('about');
 MidLink.addEventListener('click', () => {
-  this.parentElement.classList.remove('open');
+  nav.classList.remove('open');
   myOpen.classList.remove('display');
 });
 const LastLink = document.getElementById('contact');
 LastLink.addEventListener('click', () => {
-  this.parentElement.classList.remove('open');
+  nav.classList.remove('open');
   myOpen.classList.remove('display');
 });
